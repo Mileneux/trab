c4f8fa6b32e79771a1a2b3b9eb9eb1ec3ac1d74f
Milene <802.215@alunos.unigran.br>
2021-05-04 17:35:26 -0300

    traent

---
+++ src/oficina/Cliente.java
@@ -0,0 +1,35 @@
+package oficina;
+
+public class Cliente {
+    private String nome;
+    private String telefone;
+    private int idcliente;
+    
+
+    public int getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(int idcliente) {
+        this.idcliente = idcliente;
+    }
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+}
